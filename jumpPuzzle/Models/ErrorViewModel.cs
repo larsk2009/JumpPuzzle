903db42de158d2d566792d71b2b8318ae0d5f26e
Lars Koopmans <larsk2009@live.nl>
2019-02-26 11:18:21 +0100

    Initial commit
    Signed-off-by: Lars Koopmans <larsk2009@live.nl>

---
+++ jumpPuzzle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jumpPuzle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
